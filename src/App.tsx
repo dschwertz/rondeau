8c877b3e51fb8d0528e2b80fd8c0e99583a9b128
Dillon Schwertz <dschwertz800@gmail.com>
2026-02-12 23:58:58 -0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <>
+      <div className='w-screen h-screen flex justify-center items-center'>
+        <p className='text-lg'>Coming Soon...</p>
+      </div>
+    </>
+  )
+}
