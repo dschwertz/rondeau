f9c842d1ce2da4ca17e76096e3ab6f989980276a
Dillon Schwertz <dschwertz800@gmail.com>
2026-04-10 22:40:09 -0700

    Adds github workflow

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
-import { Route, Switch } from 'wouter'
+import { Redirect, Route, Switch } from 'wouter'
 import Home from './pages/Home'
 import LogIn from './pages/LogIn'
-import ConfirmSignup from './pages/ConfirmSignup'
 
 function App() {
   return (
     <>
       <Switch>
         <Route path='/login' component={LogIn} />
-        <Route path='/home' component={Home} />
-        <Route path='/confirm-signup' component={ConfirmSignup} />
+        <Route path='/' component={Home} />
 
-        <Route>404</Route>
+        <Redirect to='/login' />
       </Switch>
     </>
   )
