fd1a62e06a14d4abe5d113679d5c3ff72687cbad
Dillon Schwertz <dschwertz800@gmail.com>
2026-04-12 14:21:48 -0700

    Creates s3 bucket for frontend assets

--- cloudformation/api/lambda/lambdaDeploy.sh
+++
@@ -1,4 +0,0 @@
-aws cloudformation deploy \
-  --stack-name api-lambda \
-  --template-file ./cloudformation/api/lambda/lambda.yaml \
-  --parameter-overrides $(cat ./cloudformation/api/lambda/lambdaParameters)
