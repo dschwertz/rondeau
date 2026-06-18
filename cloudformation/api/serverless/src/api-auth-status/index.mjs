121daeea612494f05d648038f42efabe2cb21ab9
Dillon Schwertz <dschwertz800@gmail.com>
2026-06-17 21:00:15 -0700

    Updates authStatus response

--- cloudformation/api/serverless/src/api-auth-status/index.mjs
+++ cloudformation/api/serverless/src/api-auth-status/index.mjs
@@ -1,37 +1,51 @@
-import { CognitoJwtVerifier } from 'aws-jwt-verify'
+import { CognitoJwtVerifier } from "aws-jwt-verify";
 
 function getIdTokenFromCookieString(cookieString) {
-  const match = cookieString.match(/(?:^|;\s*)id_token=([^;]+)/)
-  return match ? match[1] : null
+  const match = cookieString.match(/(?:^|;\s*)id_token=([^;]+)/);
+  return match ? match[1] : null;
 }
 
 const verifier = CognitoJwtVerifier.create({
   userPoolId: process.env.COGNITO_USER_POOL_ID,
   tokenUse: "id",
   clientId: process.env.COGNITO_CLIENT_ID,
-})
-
+});
 
 export const handler = async (event) => {
-  const cookieString = event.headers?.cookie ?? event.headers?.Cookie ?? ""
-  const idToken = getIdTokenFromCookieString(cookieString)
-  console.log("idToken: ", idToken)
+  const cookieString = event.headers?.cookie ?? event.headers?.Cookie ?? "";
+  const idToken = getIdTokenFromCookieString(cookieString);
+  console.log("idToken: ", idToken);
 
   if (idToken == null) {
-    console.log("Id token not found in cookies")
-    return { statusCode: 401, body: JSON.stringify({ message: "Unauthorized" }) }
+    console.log("Id token not found in cookies");
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        isAuthenticated: false,
+        message: "Id token not found in cookies.",
+      }),
+    };
   }
 
   try {
-    const payload = await verifier.verify(idToken)
-    console.log(payload)
+    const payload = await verifier.verify(idToken);
+    console.log(payload);
     return {
       statusCode: 200,
-      body: JSON.stringify({ user: { email: payload.email } })
-    }
+      body: JSON.stringify({
+        isAuthenticated: true,
+        user: { email: payload.email },
+        message: "Authentication successfully verified.",
+      }),
+    };
   } catch (error) {
-    console.error("Token verification failed:", error.message)
-    return { statusCode: 401, body: JSON.stringify({ message: "Unauthorized" }) }
+    console.error("Token verification failed:", error.message);
+    return {
+      statusCode: 401,
+      body: JSON.stringify({
+        isAuthenticated: false,
+        message: error.message,
+      }),
+    };
   }
-
-}
+};
