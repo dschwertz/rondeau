62003d5592fcf154c18faaa1c8a68e3c812e06b5
Dillon Schwertz <dschwertz800@gmail.com>
2026-09-05 22:22:15 -0700

    Creates workflow to build images and upload to ECR

--- api/src/server.ts
+++ api/src/server.ts
@@ -2,7 +2,6 @@ import express, { type Express, type Response } from 'express'
 import cookieParser from 'cookie-parser'
 import swaggerUi from 'swagger-ui-express'
 import * as swaggerDocument from './swagger.json' with { type: 'json' }
-import authRouter from './routes/authRouter.ts'
 
 const app: Express = express()
 const port = 3000
@@ -13,10 +12,9 @@ app.set('query parser', 'extended')
 app.use(cookieParser())
 
 app.use(`/${versionId}/api`, swaggerUi.serve, swaggerUi.setup(swaggerDocument.default))
-app.use(`/${versionId}/auth`, authRouter)
 
 app.get(`/${versionId}/`, (_, res: Response) => {
-  res.send('Hello World!!')
+  res.send('Hello World!')
 })
 
 app.listen(port, () => {
